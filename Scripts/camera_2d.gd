66096d53610da6e3f638a2bf7fe489c07fe054df
kirabills <pereiraa739@gmail.com>
2025-02-21 20:19:18 -0300

    camera

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -4,9 +4,15 @@ extends Camera2D
 
 @export_category("Limites")
 @export var limite_right: Marker2D
+@export var limite_left: Marker2D
+@export var limite_up: Marker2D
+@export var limite_down: Marker2D
 
 func _ready() -> void:
 	self.limit_right = int(limite_right.position.x)
+	self.limit_left = int(limite_left.position.x)
+	self.limit_top = int(limite_up.position.y)
+	self.limit_bottom = int(limite_down.position.y)
 
 func _process(_delta: float) -> void:
 	self.position.x = player.position.x
